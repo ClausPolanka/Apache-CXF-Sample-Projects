1e5b3ada2dadf06c78f0ce796a8ff8d5299b0868
Claus Polanka <claus.polanka@chello.at>
2010-11-12 16:30:37 +0100

    added missing jax-ws annotations.

--- bookstore-prototype/src/main/java/bookstore/SupplierRegistry.java
+++ bookstore-prototype/src/main/java/bookstore/SupplierRegistry.java
@@ -1,15 +1,21 @@
 package bookstore;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 import org.xmlsoap.schemas.ws._2004._08.addressing.EndpointReferenceType;
 
-@WebService
+// @formatter:off
+@WebService(name = "SupplierRegistry",
+        	targetNamespace = "http://infosys.tuwien.ac.at/aic10/ass1/dto/supplierregistry")
 public interface SupplierRegistry {
+// @formatter:on
 
 	public static final String GERMAN_SUPPLIER_ADDRESS = "http://localhost:9000/germansupplier";
 	public static final String AUSTRIA_SUPPLIER_ADDRESS = "http://localhost:9000/austriasupplier";
 
-	EndpointReferenceType getAddressFromSupplierFor(Product aProduct);
+	@WebMethod(operationName = "get_address_from_supplier")
+	EndpointReferenceType getAddressFromSupplierFor(@WebParam(name = "product") Product aProduct);
 
 }
